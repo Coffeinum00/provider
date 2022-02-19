bbba4a06acbb077309175194db2557ca25e4f368
Coffeinum00 <coffeinum00@gmail.com>
2022-02-19 12:34:22 +0100

    three

--- lib/home/screens/home.dart
+++ lib/home/screens/home.dart
@@ -1,11 +1,27 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1902/home/provider/example_provider.dart';
 import 'package:flutter_application_1902/home/screens/example.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), body: const Example());
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Provider.of<ExampleProvider>(context).height =
+                  Random().nextInt(300).toDouble();
+              Provider.of<ExampleProvider>(context).width =
+                  Random().nextInt(300).toDouble();
+            },
+            icon: const Icon(Icons.flutter_dash),
+          ),
+        ),
+        body: const Example());
   }
 }
